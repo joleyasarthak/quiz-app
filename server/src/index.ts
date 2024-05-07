1b419405ad7d9d2f82fe873944492b9fdd3dcaab
joleyasarthak <sarthakjoleya@gmail.com>
2024-05-07 12:30:36 +0530

    netlify commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
-const express = require("express");
+import express, { Request, Response } from 'express';
 const app = express();
 require("dotenv").config();
 app.use(express.json());
 import morgan from "morgan";
 const dbConfig = require("./config/dbConfig");
 import cors from "cors";
+import serverless from "serverless-http";
 
 const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
@@ -15,8 +16,13 @@ app.use(cors());
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute);
+app.use("/api/test", (req:Request,res:Response)=>{
+  return res.status(200).send("Working!");
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+export const handler = serverless(app);
